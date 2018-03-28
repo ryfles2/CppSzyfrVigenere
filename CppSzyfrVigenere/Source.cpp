b97801653d4cf4c029f2dee534a9dbbc92c7b4ed
ryfles2 <rafalpolanin@gmail.com>
2018-03-28 20:43:00 +0200

    fix small letter

--- CppSzyfrVigenere/Source.cpp
+++ CppSzyfrVigenere/Source.cpp
@@ -1,127 +1,136 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <locale.h>
-
+#include <clocale>
 
 using namespace std;
 
-
+//lepsza bespieczna wersja oparta o kod ASCII
 
 void main()
 {
+	//polskie znaki w kosoli
 	setlocale(LC_ALL, "");
-	fstream fFiles;
-	fstream tFiles;
-	string sFrom;
-	string sWhere;
-	int x = 0;
+
+	fstream f_files;
+	fstream t_files;
+	string s_from;
+	string s_where;
 	string szyfr;
+	auto x = 0;
+
 	cout << "1.Szyfrowanie " << endl << "2.Deszyfrowanie " << endl;
 	cin >> x;
 	cout << "Nazwa pliku z którego czytamy" << endl;
-	cin >> sFrom;
+	cin >> s_from;
 	cout << "Nazwa pliku do którego zapisujemy" << endl;
-	cin >> sWhere;
+	cin >> s_where;
 	cout << "Podaj szyfr" << endl;
 	cin >> szyfr;
+	
+	//Zamiana ma³ych liter na du¿e w szyfr
+	for (auto& i : szyfr)
+	{
+		if (i > 95) i -= 32;
+	}
 
-	fFiles.open(sFrom);
-	tFiles.open(sWhere, ios::out);
-	string tString;
+	f_files.open(s_from);
+	t_files.open(s_where, ios::out);
+
+	string t_string;
 	string line;
-	if (fFiles.good())
+
+	//zczytywanie linia po lini
+	if (f_files.good())
 	{
 		std::cout << "Uzyskano dostep do pliku!" << std::endl;
-		while (getline(fFiles, line))
+		while (getline(f_files, line))
 		{
 
-			tString += line + '\n';
+			t_string += line + '\n';
 		}
 	}
 	else
 	{
 		cout << "Dostep do pliku zostal zabroniony!" << endl;
 	}
-	for (auto i = 0; i < tString.size(); i++)
+	//Zamiana ma³ych liter na du¿e w wejœciu
+	for (auto& i : t_string)
 	{
-		if (tString[i] > 95) tString[i] -= 32;
+		if (i > 95) i -= 32;
 	}
-	string tSzyfr;
-	int lSzyfr = 0;
-	for (auto i = 0; i < tString.size(); i++)
+	string t_szyfr;
+	auto l_szyfr = 0;
+	for (auto i : t_string)
 	{
-		if (48 <= tString[i] && tString[i] <= 57 || 65 <= tString[i] && tString[i] <= 90)
+		if ((48 <= i && i <= 57) || (65 <= i && i <= 90))
 		{
-			tSzyfr += szyfr[lSzyfr];
-			lSzyfr++;
-			if (lSzyfr == szyfr.size()) lSzyfr = 0;
+			t_szyfr += szyfr[l_szyfr];
+			l_szyfr++;
+			if (l_szyfr == szyfr.size()) l_szyfr = 0;
 		}
-		else tSzyfr += tString[i];
+		else t_szyfr += i;
 	}
-	cout << tSzyfr << endl;
-
-
-
+	//cout << t_szyfr << endl;
 	if (x == 1)
 	{
-		for (auto i = 0; i < tString.size(); i++)
+		for (auto i = 0; i < t_string.size(); i++)
 		{
-			if (48 <= tSzyfr[i] && tSzyfr[i] <= 57)
+			if (48 <= t_szyfr[i] && t_szyfr[i] <= 57)
 			{
-				int przes = tSzyfr[i] - 48;
-				cout << przes << endl;
+				const auto przes = t_szyfr[i] - 48;
+				//cout << przes << endl;
 				for (auto j = 0; j < przes; j++)
 				{
-					tString[i]++;
-					if (tString[i] == 58) tString[i] = 65;
-					if (tString[i] == 91) tString[i] = 48;
+					t_string[i]++;
+					if (t_string[i] == 58) t_string[i] = 65;
+					if (t_string[i] == 91) t_string[i] = 48;
 				}
 			}
-			else if (65 <= tSzyfr[i] && tSzyfr[i] <= 90)
+			else if (65 <= t_szyfr[i] && t_szyfr[i] <= 90)
 			{
-				int przes = tSzyfr[i] - 55;
-				cout << przes << endl;
+				const auto przes = t_szyfr[i] - 55;
+				//cout << przes << endl;
 				for (auto j = 0; j < przes; j++)
 				{
-					tString[i]++;
-					if (tString[i] == 58) tString[i] = 65;
-					if (tString[i] == 91) tString[i] = 48;
+					t_string[i]++;
+					if (t_string[i] == 58) t_string[i] = 65;
+					if (t_string[i] == 91) t_string[i] = 48;
 				}
 			}
 		}
 	}
 	else
 	{
-		for (auto i = 0; i < tString.size(); i++)
+		for (auto i = 0; i < t_string.size(); i++)
 		{
-			if (48 <= tSzyfr[i] && tSzyfr[i] <= 57)
+			if (48 <= t_szyfr[i] && t_szyfr[i] <= 57)
 			{
-				int przes = tSzyfr[i] - 48;
-				cout << przes << endl;
+				const auto przes = t_szyfr[i] - 48;
+				//cout << przes << endl;
 				for (auto j = 0; j < przes; j++)
 				{
-					tString[i]--;
-					if (tString[i] == 47) tString[i] = 90;
-					if (tString[i] == 64) tString[i] = 57;
+					t_string[i]--;
+					if (t_string[i] == 47) t_string[i] = 90;
+					if (t_string[i] == 64) t_string[i] = 57;
 				}
 			}
-			else if (65 <= tSzyfr[i] && tSzyfr[i] <= 90)
+			else if (65 <= t_szyfr[i] && t_szyfr[i] <= 90)
 			{
-				int przes = tSzyfr[i] - 55;
-				cout << przes << endl;
+				const auto przes = t_szyfr[i] - 55;
+				//cout << przes << endl;
 				for (auto j = 0; j < przes; j++)
 				{
-					tString[i]--;
-					if (tString[i] == 47) tString[i] = 90;
-					if (tString[i] == 64) tString[i] = 57;
+					t_string[i]--;
+					if (t_string[i] == 47) t_string[i] = 90;
+					if (t_string[i] == 64) t_string[i] = 57;
 				}
 			}
 		}
 	}
 
-	cout << tString << endl;
-	tFiles << tString;
-	fFiles.close();
-	tFiles.close();
+	cout << t_string << endl;
+	t_files << t_string;
+	f_files.close();
+	t_files.close();
 }
